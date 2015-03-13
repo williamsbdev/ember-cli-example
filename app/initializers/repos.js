cdd31bb3b3466d3f9ca7d6f15d04e66a1082ee88
Brandon Williams <williamsbdev@gmail.com>
2015-03-13 15:20:44 -0500

    [ENHANCEMENT]: first stab at auto registration

--- app/initializers/repos.js
+++ app/initializers/repos.js
@@ -1,8 +1,20 @@
 import Ember from "ember";
-import PersonRepo from "example/repos/person";
+
+var registerWithContainer = function(dirName, application) {
+    var directoryRegExp = new RegExp("^" + "example/" + dirName);
+
+    Ember.keys(requirejs._eak_seen).filter(function(key) {
+        return directoryRegExp.test(key);
+    }).forEach(function(moduleName) {
+        var module = require(moduleName, null, null, true);
+        if (!module) { throw new Error(moduleName + ' must export a single object to be registered with container.'); }
+        var fileName =  moduleName.match(/[^\/]+\/?$/)[0];
+        application.register(dirName + ":" + fileName, module['default']);
+    });
+};
 
 export function initialize(container, application) {
-    application.register("repos:person", PersonRepo);
+    registerWithContainer("repos", application);
     Ember.inject.repos = function(name) {
         return container.lookup("repos:" + name);
     };
