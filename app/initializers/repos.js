1e084c5021a430ded21e5054f76019d4f2ec999f
Brandon Williams <williamsbdev@gmail.com>
2015-03-16 06:16:43 -0500

    [REFACTOR]: cleaning up the implementation of Ember.inject

--- app/initializers/repos.js
+++ app/initializers/repos.js
@@ -4,13 +4,10 @@ import registerWithContainer from "ember-cli-auto-register/register";
 export function initialize(container, application) {
     registerWithContainer("repos", application);
     Ember.inject.repos = function(name) {
-        var value = function() {
-            return container.lookup("repos:" + name);
-        };
-        var computed = function(name) {
-            return container.lookup("repos:" + name);
-        };
-        return name ? value.property() : computed.property();
+        return function(propertyName) {
+            var objectName = name || propertyName;
+            return container.lookup("repos:" + objectName);
+        }.property();
     };
 }
 
